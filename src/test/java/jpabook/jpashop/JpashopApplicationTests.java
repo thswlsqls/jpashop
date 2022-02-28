806a91a8d096f56e8fb3ef793be11ae9010e9004
thswlsqls <thswlsqls@naver.com>
2022-02-28 23:39:54 +0900

    test - context Loading

--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JpashopApplicationTests {
+class JpashopApplicationTests { // 스프링부트 이니셜라이저 초기 코드에 포함됨
 
 	@Test
 	void contextLoads() {
