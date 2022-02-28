a855eab6e153797dd6da9900ff51c787ba001cde
thswlsqls <thswlsqls@naver.com>
2022-02-28 23:40:02 +0900

    main

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpashopApplication {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) { // 스프링부트 이니셜라이저 초기 코드에 포함됨
 		//lombok 적용 확인
 		Hello hello = new Hello();
 		hello.setData("hello");
