6480fd407a4e816560038ae4bf7d0c04ef678ecf
thswlsqls <thswlsqls@naver.com>
2022-03-02 18:32:07 +0900

    saveItem, updateItem, findItems, findOne

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +21,17 @@ public class ItemService {
         itemRepository.save(item);
     }
 
-    public List<Item> findItem(){
+    @Transactional
+    public Item updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+        // ... merge는 이상을 JPA가 자동화함, 그러나 기존 값이 유지가 안됨, 따라서 병합 대신 변경감지를 사용해 수정해야함
+        return findItem;
+    }
+
+    public List<Item> findItems(){
         return itemRepository.findAll();
     }
 
